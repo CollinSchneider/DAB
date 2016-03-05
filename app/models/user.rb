d5814fabdc3dab120be3ab75fd83971383b98402
zmess24 <zac.messinger@gmail.com>
2016-03-05 17:05:22 -0500

    altered user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(email: auth.info.email).first_or_create do |user|
+    	user.email = auth.info.email
+    	user.status = 0
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
