2c16f3d36920f82ea93bf75aff20c9f4e39b5e4c
Collin Schneider <CollinThomasSchneider@yahoo.com>
2016-03-10 15:21:18 -0500

    more address improvement

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -13,6 +13,18 @@ class AddressesController < ApplicationController
     redirect_to request.referrer
   end
 
+  def update
+    authenticate_user
+    all_addresses = Address.all
+    all_addresses.each do |address|
+      address.active = 'no'
+      address.save
+    end
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to request.referrer
+  end
+
   private
   def address_params
     params.require(:address).permit(:user_id, :name, :country, :state, :zip, :city, :address, :active)
