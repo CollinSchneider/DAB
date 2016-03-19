bb05e785fb8400c705d455803ba73fbcc9abe838
Collin Schneider <CollinThomasSchneider@yahoo.com>
2016-03-19 12:47:25 -0400

    modularizing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,20 @@
 class UserMailer < ApplicationMailer
   default from: "CollinThomasSchneider@yahoo.com"
 
-  def welcome_email(user)
+  def user_welcome_email(user)
     @user = user
-    @url = "http://localhost:3000"
-    mail(to: @user.email, subject: 'Testing welcome')
+    mail(to: @user.email, subject: 'Welcome to DealBaked!')
+  end
+
+  def affiliate_welcome_email(affiliate)
+    @user = affiliate
+    mail(to: @user.email, subject: "Welcome to DealBaked!")
   end
 
   def low_inventory_email(user, product_item)
     @user = user
     @product_item = product_item
-    mail(to: @user.email, subject: "Low inventory on dealbaked")
+    mail(to: @user.email, subject: "Low inventory on DealBaked")
   end
 
 end
