e6118b5b36ebd01f8b904fc5c90143a917e8c5ed
Collin Schneider <CollinThomasSchneider@yahoo.com>
2016-03-20 17:04:56 -0400

    unseed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,24 @@
 
 ProductItem.destroy_all
 Product.destroy_all
-
-50.times do
-  Product.create(
-    user_id: [42, 43, 44].sample,
-    status: 2,
-    category: ['apparel', 'tech', 'art_culture', 'gadgets', 'essentials', 'accessories'].sample,
-    title: Faker::Commerce.product_name,
-    price: Faker::Number.between(1, 150),
-    description: Faker::Lorem.paragraph,
-    feature_one: Faker::Lorem.sentence,
-    feature_two: Faker::Lorem.sentence,
-    feature_three: Faker::Lorem.sentence,
-    feature_four: Faker::Lorem.sentence,
-    feature_five: Faker::Lorem.sentence,
-    picture: Faker::Avatar.image,
-    total_orders: 0
-    )
-end
+#
+# 50.times do
+#   Product.create(
+#     user_id: [42, 43, 44].sample,
+#     status: 2,
+#     category: ['apparel', 'tech', 'art_culture', 'gadgets', 'essentials', 'accessories'].sample,
+#     title: Faker::Commerce.product_name,
+#     price: Faker::Number.between(1, 150),
+#     description: Faker::Lorem.paragraph,
+#     feature_one: Faker::Lorem.sentence,
+#     feature_two: Faker::Lorem.sentence,
+#     feature_three: Faker::Lorem.sentence,
+#     feature_four: Faker::Lorem.sentence,
+#     feature_five: Faker::Lorem.sentence,
+#     picture: Faker::Avatar.image,
+#     total_orders: 0
+#     )
+# end
 
 # 150.times do
 #   ProductItem.create(
