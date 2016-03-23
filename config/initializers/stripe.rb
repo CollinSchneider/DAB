17aa69970a3741b1c78c58e722a979f6acb389d5
Collin Schneider <CollinThomasSchneider@yahoo.com>
2016-03-22 22:23:26 -0400

    gem fix

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,8 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLIC_TOKEN'],
-  :secret_key      => ENV['STRIPE_SECRET_TOKEN']
+  # :publishable_key => ENV['STRIPE_PUBLIC_TOKEN']
+  :publishable_key => 'pk_test_BnTh2K2CSof6rh1MFjqlMFla',
+  # :secret_key      => ENV['STRIPE_SECRET_TOKEN']
+   :secret_key => 'sk_test_uQFhbzVPXCNfYt8RWMeCHZTl'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-ProductItem.destroy_all
-Product.destroy_all
+# ProductItem.destroy_all
+# Product.destroy_all
 #
 # 50.times do
 #   Product.create(
@@ -33,5 +33,25 @@ Product.destroy_all
 #     description: ['XL', 'Blue', 'XS', 'Large Red', 'Green', 'Medium', 'Pink'].sample,
 #     quantity: Faker::Number.between(1, 100),
 #     status: 1
+#   )
+# end
+
+# 5.times do
+#   Order.create(
+#   user_id: [59, 79].sample,
+#   address_id: [10, 8, 9].sample,
+#   pre_tax_total: [100, 150, 200].sample,
+#   tax_amount: [10, 15, 25].sample
+#   )
+# end
+
+# 10.times do
+#   OrderItem.create(
+#   order_id: [61..65].sample,
+#   user_id: [59, 79].sample,
+#   affiliate_id: [72, 75, 42],
+#   status: 0,
+#   product_item_id: 1124,
+#   quantity: [1..10].sample
 #   )
 # end
