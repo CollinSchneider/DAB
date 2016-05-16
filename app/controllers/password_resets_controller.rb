d8f269bee3686e7a69567e8be0323c21a2812654
zmess24 <zac.messinger@gmail.com>
2016-05-16 19:39:09 -0400

    updating show product page

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,6 +5,7 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
+    binding.pry
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.skip_user_validation = true
